4c8ce36be048c7e41920b734996c45c713f008e6
keertirajmalik <keertirajmalik@gmail.com>
2022-02-09 17:35:29 +0530

    Adding networking code

--- Country/ViewController.swift
+++ Country/ViewController.swift
@@ -9,12 +9,23 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var countries = [Country]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let urlString: String
         
+        urlString = "https://restcountries.com/v3.1/all"
 
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    self.parse(json: data)
+                    return
+                }
+            }
+            self.showError()
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -30,5 +41,28 @@ class ViewController: UITableViewController {
     }
 }
 
-
+extension ViewController {
+    func showError() {
+        DispatchQueue.main.async {
+            print("There was a problem loading the feed; please check your connection and try again.")
+        }
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+//        if let jsonCountry = try? decoder.decode([Country].self, from: json) {
+//            countries = jsonCountry
+//            print(countries)
+//        }
+        do {
+            let jsonCountry = try decoder.decode([Country].self, from: json)
+            countries = jsonCountry
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
 

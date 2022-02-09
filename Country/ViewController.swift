cde222de9a4f62a0a117b430ab0fc313c124296f
keertirajmalik <keertirajmalik@gmail.com>
2022-02-09 17:34:02 +0530

    adding the necessary table view metthods

--- Country/ViewController.swift
+++ Country/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let urlString: String
+        
+
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        
+        let country = countries[indexPath.row]
+        cell.textLabel?.text = country.name.common
+        return cell
+    }
 }
+
+
 
